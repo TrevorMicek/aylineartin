43b7364d00fbe5426cdd27dccf9fe5ece28df6d4
TrevorMicek <miceking@comcast.net>
2022-08-18 16:50:06 -0600

    deleted unused img link in video comp

--- src/components/tailwindComps/Video.js
+++ src/components/tailwindComps/Video.js
@@ -1,12 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import Ticker from 'react-ticker'
 
-import amazon from '../../images/download.jpeg'
-import spotify from '../../images/pics/PNG/spotify.png'
-import apple from '../../images/pics/PNG/applemusic.png'
-
-import background from '../../images/5_PerformedAt/PNG/Background.png'
-import performed from '../../images/5_Performed/PNG/Performed.png'
 const CityScroll = () => {
   const [tickerSpeed, setTickerSpeed] = useState(1000);
 
@@ -35,6 +29,13 @@ const CityScroll = () => {
     </Ticker>
 )
 }
+/*
+ <div className=" mx-auto w-44 flex flew-row items-center justify-around space-x-4">
+                <a href="https://www.amazon.com/music/player/albums/B0B5ZF67NM?ref=sr_1_1&s=dmusic&keywords=heaven+in+hell+ayline+artin&crid=HA4KADOMN0VJ&sprefix=heaven+in+hell+ayline+artin%2Cdigital-music%2C119&qid=1657776156&sr=1-1"><img src={amazon} width="40px" height="20px" className="my-auto w-9 sm:w-14 md:w-20" /></a>
+                <a href="https://open.spotify.com/album/5oj4ROdwOdmoPyd140nxCa"><img src={spotify} width="40px" height="20px" className="my-auto w-12 sm:w-14 md:w-20" /></a>
+                <a href="https://music.apple.com/us/album/heaven-in-hell-single/1633671388"><img src={apple} width="40px" height="20px" className="my-auto w-12 sm:w-14 md:w-20" /></a>
+              </div>
+*/
 
 export default function Example() {
 
@@ -52,11 +53,7 @@ className="mx-auto w-72">
 </div>
 <div className="bg-purple -mt-5 h-12 my-auto flex items-center align-end justify-between">
 
-              <div className=" mx-auto w-44 flex flew-row items-center justify-around space-x-4">
-                <a href="https://www.amazon.com/music/player/albums/B0B5ZF67NM?ref=sr_1_1&s=dmusic&keywords=heaven+in+hell+ayline+artin&crid=HA4KADOMN0VJ&sprefix=heaven+in+hell+ayline+artin%2Cdigital-music%2C119&qid=1657776156&sr=1-1"><img src={amazon} width="40px" height="20px" className="my-auto w-9 sm:w-14 md:w-20" /></a>
-                <a href="https://open.spotify.com/album/5oj4ROdwOdmoPyd140nxCa"><img src={spotify} width="40px" height="20px" className="my-auto w-12 sm:w-14 md:w-20" /></a>
-                <a href="https://music.apple.com/us/album/heaven-in-hell-single/1633671388"><img src={apple} width="40px" height="20px" className="my-auto w-12 sm:w-14 md:w-20" /></a>
-              </div>
+
             </div>
     </div>
 )
